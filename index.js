04e357e161dd3129885167d64423d2f596d61c2a
MusicalWhiskey <124202980+MusicalWhiskey@users.noreply.github.com>
2024-10-29 19:56:37 -0700

    updated dependencies

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 console.log("Riggs, Jake SBA 318");
 console.log("test");
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`We are live on port: ${PORT}`);
+  });
