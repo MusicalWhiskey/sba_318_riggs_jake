7dabd494ffca465328638ff76adf7c3d23ccfe6b
MusicalWhiskey <124202980+MusicalWhiskey@users.noreply.github.com>
2024-10-30 23:53:24 -0700

    fixing routes

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,21 @@
 import express from 'express'
 import users from '../data/users.js'
-import posts from '../data/posts.js'
-import accounts from '../data/accounts.js'
+// import posts from '../data/posts.js'
+// import accounts from '../data/accounts.js'
 
 const router = express.Router();
 
-router.get('/users', (req, res) => {
+router.get('/', (req, res) => {
     res.send(users);
 })
 
-router.get('/users/:id', (req, res) => {
-    const query = req.query
-    res.send(users);
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const user = users.find(user => user.id === parseInt(id));
+    if (user) {
+        res.send(user);
+    } else {
+        res.status(404).send({ error: 'User not found' });
+    }
 })
-
 export default router
