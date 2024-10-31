4b52dd0ee3d38bb8d2ca5f190044ce38a5602a74
MusicalWhiskey <124202980+MusicalWhiskey@users.noreply.github.com>
2024-10-30 20:28:15 -0700

    project working on refreshed system with fixed dotenv dependencies

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-console.log("Riggs, Jake SBA 318");
-console.log("test");
+import express from "express";
+import "dotenv/config";
 
-require('dotenv').config();
-const express = require('express');
 const app = express();
+// app.set("view engine", "ejs");
+app.use(express.static("public"));
 const PORT = process.env.PORT || 3000;
 
 
 
 
+//404 Error Handler
+app.use((req, res) => {
+  res.status(404).json({ error: "Resource Not found" });
+});
+
+
+//General Error Handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({ error: err.message});
+});
+
 
 app.listen(PORT, () => {
-    console.log(`We are live on port: ${PORT}`);
+    console.log(`We live on port: ${PORT}`);
   });
